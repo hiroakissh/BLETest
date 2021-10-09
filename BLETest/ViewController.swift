a553ae742ec3e5878f307780831fb009a139686e
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-09 19:53:37 +0900

    Initial Commit

---
+++ BLETest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BLETest
+//
+//  Created by HiroakiSaito on 2021/10/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
