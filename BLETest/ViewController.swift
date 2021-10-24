dfb0e34261ed5e50672a32d64cb6b439291a60df
hirokaissh <hirokai.saito1014@gmail.com>
2021-10-24 14:32:40 +0900

    テストコードの削除

--- BLETest/ViewController.swift
+++ BLETest/ViewController.swift
@@ -30,10 +30,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let sampledata = "$GPGGA,085120.307,3541.1493,N,13945.3994,E,1,08,1.0,6.9,M,35.9,M,,0000*5E"
-
-        let location = nmeaParse.parsesentence(data: sampledata)
-        print(location! as CLLocation)
         setup()
     }
 
